d54fa46e9689ca197afbe9265df33f7980cf39fc
Aabhas Sharma <aabhas.sharma@gmail.com>
2016-10-03 22:32:19 -0700

    creating a db because things are getting xomplex

---
+++ create_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+sqlite_file = 'tweet_db.sqlite'
+table_name1 = 'trend_and_tweet'
+new_field = 'my_1st_column' # name of the column
+field_type = 'INTEGER'  # column data type
+
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE trend_and_tweet (
+               id integer primary key autoincrement,
+               timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+               trend varchar(50),
+               tweet varchar(160))''')
+
+# Committing changes and closing the connection to the database file
+conn.commit()
+conn.close()
