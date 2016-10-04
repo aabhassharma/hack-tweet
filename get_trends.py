f7b1244a5945fa831621c4ceedc6acc5e1909a39
Aabhas Sharma <aabhas.sharma@gmail.com>
2016-10-03 23:20:25 -0700

    getting trends and storing in db for post processing

--- get_trends.py
+++ get_trends.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import twitter
+import sqlite3
 
 def get_twitter_creds():
     '''
@@ -31,15 +32,18 @@ def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
 
-if __name__ == "__main__":
-    try:
-        twitter_creds = get_twitter_creds()
-    except KeyError:
-        print "You fool you didn't set the env vars"
-        sys.exit(1)
-    api = get_twitter_api(twitter_creds=twitter_creds)
+def write_to_tweet_table(cursor, trend, tweet_id, tweet):
+    trend_column = 'trend'
+    tweet_column = 'tweet'
+    tweet_id_column = 'tweet_id'
+    cursor.execute(
+        '''INSERT OR IGNORE INTO trend_and_tweet (trend, tweet_id, tweet) VALUES ("{}", {}, "{}")'''.format(
+            trend, tweet_id, tweet
+        ))
+
+def set_trends_and_tweets(api, cursor):
+    trends_and_tweets = []
     trending_articles = api.GetTrendsCurrent()
-    # Now we have the trends
     for trend in trending_articles:
         # I don't want the bullshit that has any promoted content with it
         if trend.promoted_content:
@@ -51,14 +55,38 @@ if __name__ == "__main__":
         if not is_ascii(trend.name):
             continue
         # Find the most popular tweets that are mentioning this trend
-        # But I also want to limit it to within 100 miles of my location
+        # But I also want to limit it to within 500 miles of my location
         searched_tweets = api.GetSearch(
             term=trend.name,
-            geocode="37.774929,-122.419416,100mi",
+            geocode="37.774929,-122.419416,500mi",
             result_type="popular")
         # Got empty list, skip
         if not searched_tweets:
             continue
         # VOILA! You have the most popular tweets within 100 miles of my location
         # that are referencing the most popular trending hashtags right Now
-        print searched_tweets
+        for status in searched_tweets:
+            # skip truncated text ugh API constraints
+            if status.truncated:
+                continue
+            write_to_tweet_table(cursor, trend.name, status.id, status.text.encode('utf-8'))
+        # import ipdb; ipdb.set_trace()
+        # print "tweets:"
+        # print searched_tweets
+        # # need the id, text
+        # print "trend:"
+        # print trend.name
+
+if __name__ == "__main__":
+    sqlite_file = 'tweet_db.sqlite'
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+    try:
+        twitter_creds = get_twitter_creds()
+    except KeyError:
+        print "You fool you didn't set the env vars"
+        sys.exit(1)
+    api = get_twitter_api(twitter_creds=twitter_creds)
+    set_trends_and_tweets(api, c)
+    conn.commit()
+    conn.close()
